8fb8064db01c146c749f8952ad7d017842206b47
Andrés Saam <andressaa.dev@gmail.com>
2019-11-26 18:59:37 -0500

    Using events handling with state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,36 +1,111 @@
-import React from 'react'
+import React from "react";
 
 import {
   FaHeart,
   FaRegHeart,
+  FaRegCommentDots,
   FaRegComment,
+  FaPaperPlane,
   FaRegPaperPlane
 } from "react-icons/fa";
 
-import './styles/Card.css'; 
+import "./styles/Card.css";
 
 class Card extends React.Component {
-    render() {
-        return (
-          <div className='Card__container'>
-            <div className='Card__container-image'>
-              <img src='http://placeimg.com/300/200/any' alt='Card' />
-            </div>
-            <div className='Card__container-text'>
-              <h2>{this.props.cardName}</h2>
-              <p>{this.props.jobTitle}</p>
-              <p>{this.props.cardDescription}</p>
-            </div>
-            <div className='Card__container-social'>
-              <h2>
-                <FaRegHeart />
-                <FaRegComment />
-                <FaRegPaperPlane />
-              </h2>
-            </div>
-          </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false,
+      commented: false,
+      shared: false
+    };
+  }
+
+  handleIconClick = e => {
+    //console.log(e.target);
+    //console.log(document.querySelector(".like path"));
+    let likeIconPath = document.querySelector(".like path");
+    likeIconPath.classList.add("like");
+    let commentIconPath = document.querySelector(".comment path");
+    commentIconPath.classList.add("comment");
+    let shareIconPath = document.querySelector(".share path");
+    shareIconPath.classList.add("share");
+    switch (e.target.className.animVal) {
+      case "like":
+        this.setState({
+          liked: !this.state.liked
+        });
+        break;
+      case "comment":
+        this.setState({
+          commented: !this.state.commented
+        });
+        break;
+      case "share":
+        this.setState({
+          shared: !this.state.shared
+        });
+        break;
+      default:
+        break;
     }
+  };
+
+  render() {
+    return (
+      <div className="Card__container">
+        <div className="Card__container-image">
+          <img src="http://placeimg.com/300/200/any" alt="Card" />
+        </div>
+        <div className="Card__container-text">
+          <h2>{this.props.cardName}</h2>
+          <p>{this.props.jobTitle}</p>
+          <p>{this.props.cardDescription}</p>
+        </div>
+        <div className="Card__container-social">
+          {this.state.liked ? (
+            <FaHeart
+              className="like"
+              color="tomato"
+              onClick={this.handleIconClick.bind(this)}
+            />
+          ) : (
+            <FaRegHeart
+              className="like"
+              color="tomato"
+              onClick={this.handleIconClick.bind(this)}
+            />
+          )}
+          {this.state.commented ? (
+            <FaRegCommentDots
+              className="comment"
+              color="royalblue"
+              onClick={this.handleIconClick.bind(this)}
+            />
+          ) : (
+            <FaRegComment
+              className="comment"
+              color="royalblue"
+              onClick={this.handleIconClick.bind(this)}
+            />
+          )}
+          {this.state.shared ? (
+            <FaPaperPlane
+              className="share"
+              color="forestgreen"
+              onClick={this.handleIconClick.bind(this)}
+            />
+          ) : (
+            <FaRegPaperPlane
+              className="share"
+              color="forestgreen"
+              onClick={this.handleIconClick.bind(this)}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
