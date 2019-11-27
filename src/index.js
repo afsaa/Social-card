495429144108bc70d41b44b37fa6ef63edc39868
Andrés Saam <andressaa.dev@gmail.com>
2019-11-27 16:19:16 -0500

    Rendering the CardList component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 //import App from './App';
-import Card from './components/Card';
-import * as serviceWorker from './serviceWorker';
+//import Card from "./components/Card";
+import CardList from "./components/CardList";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Card cardName="Andrés Saa" jobTitle="Frontend Engineer" cardDescription="Lorem ipsum dolor ..." />, document.getElementById('root'));
+ReactDOM.render(<CardList />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
